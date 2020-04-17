259f2a76a77bc8e127dbc97017fc406d0551782e
Beau Troxclair <beau.troxclair@gmail.com>
2020-04-17 16:34:40 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,10 +11,10 @@ def fourth_challenge
          :attitude => "worried"
        },
        {
-         :name => "Benvolio",
-         :age => "17",
-         :attitude => "worried"
-       },
+         :name => "Mercutio",
+         :age => "18",
+         :attitude => "hot-headed"
+       }
        
       ]
    }, 
@@ -23,7 +23,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {
+         :name => "Steven",
+         :age => "30",
+         :attitude => "confused"
+       },
+       {
+         :name => "Nurse",
+         :age => "44",
+         :attitude => "worried"
+       }
       ]
    }
 }
